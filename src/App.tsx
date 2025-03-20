bdb8883f423979c946ae6de901ca1dc56014ce75
Lukasz <lukasz.maniak22@gmail.com>
2025-03-20 20:39:11 +0100

    filesStructureSetup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import words from "./wordList.json";
+import HangmanDrawing from "./HangmanDrawing";
+
+import Keyboard from "./Keyboard";
+import HangmanWordToGuess from "./HangmanWordToGuess";
+
 function App() {
-  return <></>;
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div className="flex w-full content-center gap-x-10">
+      <h1 className="text-9xl font-bold">
+        HANG
+        <br />
+        MAN
+      </h1>
+
+      <div className="flex flex-col items-start gap-y-10">
+        <HangmanDrawing />
+        <HangmanWordToGuess />
+        <Keyboard />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/HangmanWordToGuess.tsx
@@ -0,0 +1,3 @@
+export default function HangmanWordToGuess() {
+  return <div>HangmanWordToGuess</div>;
+}
